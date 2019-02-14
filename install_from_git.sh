3dd0d6e106969c0dd1baf6b373d10e35999b7f8d
David Vazquez Garcia <david.vazquezgijon@gmail.com>
2019-02-14 17:47:34 +0100

    Scripts for installation from GIT and configuration added.

---
+++ install_from_git.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/zsh
+
+
+# Define GIT repositories to clone
+#repos=('https://github.com/husky/husky.git' \
+       #'https://github.com/ros-drivers/um6.git' \
+       #'https://github.com/ros-teleop/teleop_twist_joy.git' \
+       #'https://github.com/ros-perception/openslam_gmapping' \
+       #'https://github.com/ros-perception/slam_gmapping' \
+       #'https://github.com/clearpathrobotics/cpr_multimaster_tools.git' \
+       #'https://github.com/ros-drivers/nmea_comms.git' \
+       #'https://github.com/ros-perception/imu_pipeline.git' \
+       #'https://github.com/ros-drivers/microstrain_3dmgx2_imu.git' \
+       #'https://github.com/paulbovbel/frontier_exploration.git' \
+        #)
+repos=( \
+    'https://github.com/husky/husky.git' \
+    'https://github.com/davidvg/LMS1xx.git' \
+    'https://github.com/ros-visualization/interactive_marker_twist_server.git' \
+    )
+
+ROSDEP_INSTALL=false
+
+echo "Inititating workspace..."
+# Make SRC_DIR and enter it
+mkdir -p ./src
+cd ./src
+
+# Clone the repositories defined in repos
+echo "\nClone GIT repositories..."
+for repo in $repos
+do
+    args=""
+    repo_name=$(echo $repo | cut -d "/" -f 5 | cut -d "." -f 1)
+    if [ ! -d $repo_name ]
+    then
+        git clone $args $repo
+    else
+        echo "Repository $repo already exists."
+    fi
+done
+echo "\nDone."
+
+cd ..
+
+
+# Install packages using rosdep
+if [ $ROSDEP_INSTALL = true ]
+then
+    echo "\nInstalling packages..."
+    rosdep install --from-paths . -i
+fi
+
+echo "\nCompiling code..."
+catkin build
+
+source ./load_env.sh
---
+++ load_env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/zsh
+
+# Setup file
+SETUP_FILE=./devel/setup.zsh
+
+# Turtlebot3 model
+export TURTLEBOT3_MODEL=burger
+echo "Exported: TURTLEBOT3_MODEL=$TURTLEBOT3_MODEL"
+
+# Gazebo description file
+export HUSKY_GAZEBO_DESCRIPTION=$(rospack find husky_gazebo)/urdf/description.gazebo.xacro
+echo "Exported: HUSKY_GAZEBO_DESCRIPTION=$HUSKY_GAZEBO_DESCRIPTION"
+
+#  Environment variables for correct visualization
+export QT_AUTO_SCREEN_SCALE_FACTOR=
+export QT_SCREEN_SCALE_FACTORS=
+
+source $SETUP_FILE 
+echo "$SETUP_FILE loaded."
